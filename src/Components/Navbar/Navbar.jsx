61232d9bd26a7845d62b10c78baae0d86209e32c
bmdjtt <thinhnguyenduc1512@gmail.com>
2024-05-23 18:02:37 +0700

    lam navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Navbar.css"
+import logoWeb from '../../assets/logo_v4.png'
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <img src= {logoWeb} alt="" className='logo'/>
+        <ul>
+            <li>CALCULATE</li>
+            <li>DIAMONDS CHECK</li>
+            <li>APPRAISAL</li>
+        </ul>
+        <div>
+            <ul>
+                <li>VAULT</li>
+                <button>Sign in</button>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
