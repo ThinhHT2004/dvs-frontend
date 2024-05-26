bafc3253a082a47b70d07263dbe0c18e059f2a48
Tan Thinh <thinhhtse183224@fpt.edu.vn>
2024-05-26 21:58:15 +0700

    Add Router for App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import {BrowserRouter, Route, Routes, } from 'react-router-dom'
+import HomePage from './Components/homepage/HomePage'
+import DiamondAppraisal from './Components/appraisal/DiamondAppraisal'
 const App = () => {
   return (
-    <div className='Container'>
-        <Navbar/>
-    </div>
+    <>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage></HomePage>}></Route>
+        <Route path='/diamond-appraisal' element={<DiamondAppraisal></DiamondAppraisal>}></Route>
+      </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
