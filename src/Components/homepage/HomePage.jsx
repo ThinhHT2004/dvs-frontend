430374ecad2034ddf9df5559c2dbd0031ddf6376
Tan Thinh <thinhhtse183224@fpt.edu.vn>
2024-05-26 21:58:52 +0700

    Create HomePage.jsx

---
+++ src/Components/homepage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar'
+
+const HomePage = () => {
+  return (
+    <div>
+        <Navbar></Navbar>
+    </div>
+  )
+}
+
+export default HomePage
