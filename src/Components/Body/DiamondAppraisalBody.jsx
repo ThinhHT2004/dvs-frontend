8562dd2ed24d07f3e031f3e8f48bc93de576b04c
bmdjtt <thinhnguyenduc1512@gmail.com>
2024-05-27 23:05:36 +0700

    background

---
+++ src/Components/Body/DiamondAppraisalBody.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './DiamondAppraisalBody.css'
+import backgroundBody from '../../assets/BackGround.png'
+const DiamondAppraisalBody = () => {
+  return (
+    <div>
+        <img src={backgroundBody} alt="" className='background'/>
+    </div>
+  )
+}
+
+export default DiamondAppraisalBody
