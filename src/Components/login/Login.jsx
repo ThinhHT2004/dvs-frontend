13a16a1f988072ab555f2173d8232aca5461d241
Tan Thinh <thinhhtse183224@fpt.edu.vn>
2024-05-28 17:25:22 +0700

    Update Login.jsx
    Add Login Function. Call login API

--- src/Components/login/Login.jsx
+++ src/Components/login/Login.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../Navbar/Navbar'
 import { Box, Grid, Paper, TextField , Button, Typography, Link } from '@mui/material'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 
 const Login = () => {
 
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const navigator = useNavigate();
+
+    const [status, setStatus] = useState('');
+
     const paperStyle = {padding: 20, height: '70vh', width: 350, margin: "20px auto", marginTop: '150px'}
+
+    function handleLogin(){
+        axios
+        .post('http://localhost:8080/api/accounts/login', {username: username, password: password})
+        .then(response => {
+            if(response.data.status === true){
+                sessionStorage.setItem('username', username);
+                navigator('/');
+            }
+        }        
+        )
+        .catch(error => alert(error))
+    }
   return (
     <div>
 
@@ -17,13 +39,13 @@ const Login = () => {
                         <h1>Sign In</h1>
                     </Box>
                     <Box mt={6}>
-                    <TextField label='Username' placeholder='Username' fullWidth required variant='standard' margin='normal'></TextField>
+                    <TextField label='Username' placeholder='Username' fullWidth required variant='standard' margin='normal' value={username} onChange={(e) => setUsername(e.target.value)}></TextField>
                     </Box>
                     <Box>
-                        <TextField label='Password' placeholder='Password' fullWidth required variant='standard' margin='normal'></TextField>
+                        <TextField label='Password' placeholder='Password' fullWidth required variant='standard' margin='normal' value={password} onChange={(e) => setPassword(e.target.value)}></TextField>
                     </Box>
                     <Box mt={3}>
-                        <Button variant="contained" sx={{background: '#69CEE2', borderRadius: '8px'}}>Sign In</Button>
+                        <Button variant="contained" sx={{background: '#69CEE2', borderRadius: '8px'}} onClick={() => handleLogin()}>Sign In</Button>
                     </Box>
 
                     <Typography>
