02c03e7fe55af336e3df0deaa67fb124880de86f
bmdjtt <thinhnguyenduc1512@gmail.com>
2024-05-28 14:50:00 +0700

    background add

--- src/Components/Body/DiamondAppraisalBody.jsx
+++ src/Components/Body/DiamondAppraisalBody.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import './DiamondAppraisalBody.css'
-import backgroundBody from '../../assets/BackGround.png'
+
 const DiamondAppraisalBody = () => {
   return (
-    <div>
-        <img src={backgroundBody} alt="" className='background'/>
+    <div className='body'>
     </div>
   )
 }
