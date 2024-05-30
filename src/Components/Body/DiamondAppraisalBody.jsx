c5a65f802f6b259b468e78ab16a4b99ba907b506
Truong Thinh <thinhndtse182967@fpt.edu.vn>
2024-05-30 15:38:57 +0700

    complete the appraisal

--- src/Components/Body/DiamondAppraisalBody.jsx
+++ src/Components/Body/DiamondAppraisalBody.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './DiamondAppraisalBody.css'
-import { Link } from '@mui/material'
 import UserDiamondAppraisalBody from './UserDiamondAppraisalBody'
 
 const DiamondAppraisalBody = () => {
