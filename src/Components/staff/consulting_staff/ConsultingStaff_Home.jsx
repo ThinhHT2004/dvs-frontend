6bc45b036a49da42a3f9d539b20090d622a5e622
Tan Thinh <thinhhtse183224@fpt.edu.vn>
2024-05-30 15:43:35 +0700

    Update ConsultingStaff_Home.jsx

--- src/Components/staff/consulting_staff/ConsultingStaff_Home.jsx
+++ src/Components/staff/consulting_staff/ConsultingStaff_Home.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
-import StaffDrawer from '../StaffDrawer'
-
+import React from "react";
+import StaffDrawer from "../StaffDrawer";
+import {
+  Box,
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
 
 const ConsultingStaff_Home = () => {
+  const drawerWidth = 240;
   return (
     <div>
-        <StaffDrawer mylist={['Home', 'Incomming Request', 'Manage Request', 'Report', 'Form', 'Sign Out']} state='Home'></StaffDrawer>
+      <Box sx={{ display: "flex" }}>
+        <StaffDrawer
+          mylist={[
+            "Home",
+            "Incomming Request",
+            "Manage Request",
+            "Report",
+            "Form",
+            "Sign Out",
+          ]}
+          state="Home"
+        ></StaffDrawer>
+        
+      </Box>
     </div>
-  )
-}
+  );
+};
 
-export default ConsultingStaff_Home
+export default ConsultingStaff_Home;
