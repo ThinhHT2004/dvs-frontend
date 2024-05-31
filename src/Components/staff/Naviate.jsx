9f9859bebfb89c72d745843c44fe2cadfab6fa04
Tan Thinh <thinhhtse183224@fpt.edu.vn>
2024-05-31 15:12:01 +0700

    Update Naviate.jsx, Update ConsultingStaff_ManageRequest

--- src/Components/staff/Naviate.jsx
+++ src/Components/staff/Naviate.jsx
@@ -6,7 +6,7 @@ const Naviate = () => {
 
 export default Naviate;
 
-export const consulting__staff_navigator = (text, navigate) => {
+export const consulting_staff_navigator = (text, navigate) => {
   switch (text) {
     case "Home":
       navigate("/consulting-staff/home");
@@ -23,6 +23,31 @@ export const consulting__staff_navigator = (text, navigate) => {
     case "Form":
       navigate("consulting-staff/form");
       break;
+    case "Sign Out":
+      handleSignOut(navigate);
+      break;
+  }
+};
+
+export const valuation_staff_navigator = (text, navigate) => {
+  switch (text) {
+    case "Home":
+      navigate("/valuation-staff/home");
+      break;
+    case "Diamonds Appraisal":
+      navigate("/valuation-staff/diamonds-appraisal");
+      break;
+    case "Sign Out":
+      handleSignOut(navigate);
+      break;
+  }
+};
+
+export const manager_navigator = (text, navigate) => {
+  switch (text) {
+    case 'Home': navigate('/manager/home'); break;
+    case "Pending Request": navigate('/manager/pending-request'); break;
+    case 'Receipt Management': navigator('/manager/receipt-management'); break; 
     case "Sign Out":
       handleSignOut(navigate);
       break;
--- src/Components/staff/consulting_staff/ConsultingStaff_ManageRequest.jsx
+++ src/Components/staff/consulting_staff/ConsultingStaff_ManageRequest.jsx
@@ -1,28 +1,61 @@
-import { Box } from '@mui/material'
-import React from 'react'
-import StaffDrawer from '../StaffDrawer'
-import { consulting_staff_navigator } from '../Naviate'
+import { Box, Paper, Table, TableCell, TableContainer, TableHead } from "@mui/material";
+import React from "react";
+import StaffDrawer from "../StaffDrawer";
+import { consulting_staff_navigator } from "../Naviate";
 
 const ConsultingStaff_ManageRequest = () => {
+  const drawerWidth = 240;
   return (
     <div>
+      <Box sx={{ display: "flex" }}>
         <Box>
-            <Box>
-                <StaffDrawer mylist={[
-            "Home",
-            "Incomming Request",
-            "Manage Request",
-            "Report",
-            "Form",
-            "Sign Out",
-          ]}
-          state="Manage Request"
-          handleClick={consulting_staff_navigator}
+          <StaffDrawer
+            mylist={[
+              "Home",
+              "Incomming Request",
+              "Manage Request",
+              "Report",
+              "Form",
+              "Sign Out",
+            ]}
+            state="Manage Request"
+            handleClick={consulting_staff_navigator}
           ></StaffDrawer>
-            </Box>
         </Box>
+        <Box sx={{
+            flexGrow: 1,
+            p: 3,
+            width: { sm: `calc(100% - ${drawerWidth}px)` },
+            marginTop: "5%",
+            display: "flex",
+            justifyContent: "center",
+          }}>
+          <TableContainer component={Paper} sx={{ width: 1000 }}>
+            <Table sx={{ minWidth: 700, borderRadius: 10 }}>
+              <TableHead sx={{ backgroundColor: "#69CEE2" }}>
+                <TableCell>
+                  Request ID
+                </TableCell>
+                <TableCell>
+                  Customer Name
+                </TableCell>
+                <TableCell>
+                  Service
+                </TableCell>
+                <TableCell>
+                  Quantity
+                </TableCell>
+                <TableCell>
+                  Appointment Date
+                </TableCell>
+                <TableCell></TableCell>
+              </TableHead>
+            </Table>
+          </TableContainer>
+        </Box>
+      </Box>
     </div>
-  )
-}
+  );
+};
 
-export default ConsultingStaff_ManageRequest
+export default ConsultingStaff_ManageRequest;
