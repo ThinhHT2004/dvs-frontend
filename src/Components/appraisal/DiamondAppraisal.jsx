9236a3eff242fe1e668d837eee4a2455b9c98615
bmdjtt <thinhndtse182967@fpt.edu.vn>
2024-06-20 18:50:40 +0700

    fix Diamond Appraisal

--- src/Components/appraisal/DiamondAppraisal.jsx
+++ src/Components/appraisal/DiamondAppraisal.jsx
@@ -17,7 +17,9 @@ import tickXanh from '../../assets/tichxanh.png';
 import background from '../../assets/diamondAppraisalBackground.jpg';
 import axios from 'axios';
 import Footer from '../footer/Footer';
-
+import diaApp01 from '../../assets/diamondAppraisal01.png';
+import diaApp02 from '../../assets/diamondAppraisal02.png';
+import exampleReport from '../../assets/exampleReport.png';
 const UserDiamondAppraisalBody = () => {
   const username = sessionStorage.getItem("username");
   const customerId = sessionStorage.getItem("customerId");
@@ -97,8 +99,20 @@ const UserDiamondAppraisalBody = () => {
 
   if (username === null) {
     return (
-      <Box>
-        <Typography style={{ textAlign: 'center' }}>
+      <Box margin={4}>
+        <Typography
+          sx={{
+            fontSize: {
+              xs: '16px',
+              sm: '18px',
+              md: '20px',
+              lg: '22px',
+              xl: '24px',
+            },
+            textAlign: 'center',
+            color: '#989898',
+          }}
+        >
           Please <span>
             <Link href="/accounts/signin" underline='none' sx={{ color: '#69CEE2', textDecoration: 'underline' }}>
               Sign in
@@ -120,7 +134,7 @@ const UserDiamondAppraisalBody = () => {
         ) : (
           <Grid container spacing={2} justifyContent="center">
             <Grid item xs={12}>
-              <FormControl fullWidth sx={{ backgroundColor: '#fff', fontSize: '20px' ,borderRadius:'4px'}}>
+              <FormControl fullWidth sx={{ backgroundColor: '#fff', fontSize: '20px', borderRadius: '4px' }}>
                 <Select
                   id="service-select"
                   value={service}
@@ -136,7 +150,7 @@ const UserDiamondAppraisalBody = () => {
                     borderRadius: '4px',
                     '& .MuiSelect-select': {
                       textAlign: 'left',
-                      
+
                     },
                   }}
                 >
@@ -169,14 +183,14 @@ const UserDiamondAppraisalBody = () => {
                     </InputAdornment>
                   ),
                 }}
-                sx={{ backgroundColor: '#fff', fontSize: '20px'  ,borderRadius:'4px'}}
+                sx={{ backgroundColor: '#fff', fontSize: '20px', borderRadius: '4px' }}
               />
             </Grid>
             <Grid item xs={12}>
               <LocalizationProvider dateAdapter={AdapterDateFns}>
-              
-              <DateTimePicker
-              
+
+                <DateTimePicker
+
                   inputFormat="dd/MM/yyyy hh:mm aa"
                   value={date}
                   onChange={(newValue) => setDate(newValue)}
@@ -186,7 +200,7 @@ const UserDiamondAppraisalBody = () => {
                       fullWidth
                       variant="outlined"
                       placeholder="dd/MM/yyyy hh:mm aa"
-                      sx={{ backgroundColor: "#fff", fontSize: "20px" ,borderRadius:'4px'}}
+                      sx={{ backgroundColor: "#fff", fontSize: "20px", borderRadius: '4px' }}
                     />
                   )}
                 ></DateTimePicker>
@@ -219,32 +233,80 @@ const UserDiamondAppraisalBody = () => {
 const DiamondAppraisal = () => {
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-      <Box sx={{flexGrow: 1 , zIndex: 2}}>
+      <Box sx={{ flexGrow: 1, zIndex: 2 }}>
         <Navbar />
       </Box>
-      <Box 
-      sx={{ flexGrow: 1 , backgroundImage: `url(${background})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' , zIndex: 1}}
+      <Box
+        sx={{ flexGrow: 1, backgroundImage: `url(${background})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', zIndex: 1 }}
       >
 
-        <Card variant='outlined' sx={{ borderRadius: 0, border: 0, margin: 5, background: 0}} >
+        <Card variant='outlined' sx={{ borderRadius: 0, border: 0, margin: 5, background: 0 }} >
 
           <CardHeader
-            title="REQUEST AN APPOINTMENT"
+            title={
+              <Typography
+                variant="h3"
+                sx={{
+                  fontSize: {
+                    xs: '1.5rem',
+                    sm: '1.5rem',
+                    md: '2rem',
+                    lg: '2.5rem',
+                    xl: '3rem',
+                  }
+                }}
+              >
+                REQUEST AN APPOINTMENT
+              </Typography>
+            }
           />
           <CardContent>
             <Box>
-              <Box padding={1}>
-                <Typography>
+              <Box padding={1} >
+                <Typography
+                  sx={{
+                    fontSize: {
+                      xs: '16px',
+                      sm: '18px',
+                      md: '20px',
+                      lg: '22px',
+                      xl: '24px',
+                    },
+                    color: '#989898'
+                  }}
+                >
                   You can request an appraisal appointment by submitting the form below and we will promptly with my appointment availability.
                 </Typography>
               </Box>
               <Box padding={1}>
-                <Typography>
+                <Typography
+                  sx={{
+                    fontSize: {
+                      xs: '16px',
+                      sm: '18px',
+                      md: '20px',
+                      lg: '22px',
+                      xl: '24px',
+                    },
+                    color: '#989898'
+                  }}
+                >
                   You can also email us directly at : <span style={{ color: '#69CEE2' }}>diasecurappraiser@gmail.com</span>
                 </Typography>
               </Box>
               <Box padding={1}>
-                <Typography>
+                <Typography
+                  sx={{
+                    fontSize: {
+                      xs: '16px',
+                      sm: '18px',
+                      md: '20px',
+                      lg: '22px',
+                      xl: '24px',
+                    },
+                    color: '#989898'
+                  }}
+                >
                   You can also set up an appointment by calling us directly at : <span style={{ color: '#69CEE2' }}>(+84)84913-5986</span>
                 </Typography>
               </Box>
@@ -255,40 +317,48 @@ const DiamondAppraisal = () => {
           </CardContent>
         </Card>
       </Box>
-      <Box sx={{flexGrow: 1}}>
-        <Card variant='outlined' sx={{ borderRadius: 0, border: 0}}>
+      <Box sx={{ flexGrow: 1 }}>
+        <Card variant='outlined' sx={{ borderRadius: 0, border: 0 }}>
           <CardHeader
             title="The Appraisal Process"
+            titleTypographyProps={{ variant: 'h4' }}
           />
           <CardContent>
             <Card variant='outlined' sx={{ borderRadius: 0, border: 0 }}>
               <CardHeader
-                title="What Happens First?"
+                title="What Happens First ?"
               />
               <CardContent>
+
                 <Typography>
                   When you arrive for your appointment you will need to fill out an intake form that asks some brie questions. I will then explain a little about the appraisal process and begin the physical evaluation of your jewelry item(s). During the appraisal, you sit directly across from me and your jewelry items are always in plain view. The physical evaluation of your jewelry items includes measuring, testing, note taking, calculating and photography. As I am conducting the inspection of your jewelry, I will explain to you what I am doing and answer any questions or concerns you may have during the process.
                 </Typography>
+
               </CardContent>
             </Card>
           </CardContent>
         </Card>
       </Box>
-      <Box sx={{flexGrow: 1}}>
+      <Box sx={{ flexGrow: 1 }}>
         <Card variant='outlined' sx={{ borderRadius: 0, border: 0 }}>
           <CardHeader
             title="The Evaluation Process"
+            titleTypographyProps={{ variant: 'h4' }}
           />
           <CardContent>
-            
+            <Grid container spacing={2} >
+              <Grid item md={12} xl={6}>
                 <Card variant='outlined' sx={{ borderRadius: 0, border: 0 }}>
+
                   <CardHeader
                     title="Clarity Grading :"
                   />
                   <CardContent>
+
                     <Typography>
                       The internal characteristic or inclusions of your diamond will be observed under a high powered gemological microscope and a plot diagram will show the location and type of inclusion. A clarity grade using the GIA clarity grading system will be assigned and noted in the report.
                     </Typography>
+
                   </CardContent>
                 </Card>
                 <Card variant='outlined' sx={{ borderRadius: 0, border: 0 }}>
@@ -297,7 +367,9 @@ const DiamondAppraisal = () => {
                   />
                   <CardContent>
                     <Typography>
-                      The amount of inherent color in your diamond will be evaluated and compared to GIA master graded diamonds under proper daylight diamond grading lamps. A letter grade from D to Z will be assigned based on the amount of color or (lack there of) your diamond is exhibiting. The color grade assigned will use the GIA's color grading system and noted in the report. * I have a full set of master graded diamonds used for comparison that have been graded and certified by the Gemological Institute of America (GIA).                    </Typography>
+                      The amount of inherent color in your diamond will be evaluated and compared to GIA master graded diamonds under proper daylight diamond grading lamps. A letter grade from D to Z will be assigned based on the amount of color or (lack there of) your diamond is exhibiting. The color grade assigned will use the GIA's color grading system and noted in the report. * I have a full set of master graded diamonds used for comparison that have been graded and certified by the Gemological Institute of America (GIA).
+                    </Typography>
+
                   </CardContent>
                 </Card>
                 <Card variant='outlined' sx={{ borderRadius: 0, border: 0 }}>
@@ -305,18 +377,46 @@ const DiamondAppraisal = () => {
                     title="Carat Weight :"
                   />
                   <CardContent>
+
                     <Typography>
-                      If your diamond is mounted, the carat weight is estimated by using a standard formula for the type of cut for your stone. If your stone un-mounted, it is directly weighed on a digital carat weight scale. The weight is noted in the report.                    </Typography>
+                      If your diamond is mounted, the carat weight is estimated by using a standard formula for the type of cut for your stone. If your stone un-mounted, it is directly weighed on a digital carat weight scale. The weight is noted in the report.
+                    </Typography>
+
                   </CardContent>
+
                 </Card>
+              </Grid>
+              <Grid
+                item
+                md={12}
+                xl={6}
+                container
+                direction="column"
+                justifyContent="center"
+                alignItems="center">
+                <Box >
+                  <img src={diaApp01} alt="" style={{ width: '600px', height: '450px' }} />
+                </Box>
+              </Grid>
+            </Grid>
+
+            <Grid container spacing={2} marginTop={1}>
+
+              <Grid
+                item
+                xl={6}
+                md={12}
+              >
                 <Card variant='outlined' sx={{ borderRadius: 0, border: 0 }}>
                   <CardHeader
                     title="Finish :"
                   />
                   <CardContent>
+
                     <Typography>
                       Symmetry and Polish will be assessed and given a grade of Poor to Excellent and noted in the report.
                     </Typography>
+
                   </CardContent>
                 </Card>
                 <Card variant='outlined' sx={{ borderRadius: 0, border: 0 }}>
@@ -324,9 +424,11 @@ const DiamondAppraisal = () => {
                     title="Cutting Proportions :"
                   />
                   <CardContent>
+
                     <Typography>
                       The cutting proportions of your diamond are evaluated. This includes, measurements of the diameter or length & width, along with the depth of your diamond. Depth%, Table%, Crown Angle, and Girdle Thickness are evaluated and noted in the final report.
                     </Typography>
+
                   </CardContent>
                 </Card>
                 <Card variant='outlined' sx={{ borderRadius: 0, border: 0 }}>
@@ -334,9 +436,11 @@ const DiamondAppraisal = () => {
                     title="Cut Grade :"
                   />
                   <CardContent>
+
                     <Typography>
                       Any and all enhancements that are detectable by gemological testing and or observation will be noted in the report.
                     </Typography>
+
                   </CardContent>
                 </Card>
                 <Card variant='outlined' sx={{ borderRadius: 0, border: 0 }}>
@@ -344,19 +448,42 @@ const DiamondAppraisal = () => {
                     title="Enhancements :"
                   />
                   <CardContent>
+
                     <Typography>
                       The internal characteristic or inclusions of your diamond will be observed under a high powered gemological microscope and a plot diagram will show the location and type of inclusion. A clarity grade using the GIA clarity grading system will be assigned and noted in the report.
                     </Typography>
+
                   </CardContent>
                 </Card>
+              </Grid>
+              <Grid
+                item
+                xl={6}
+                md={12}
+                container
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+              >
+                <img src={diaApp02} alt="" />
+              </Grid>
+            </Grid>
+            <Grid container spacing={2} marginTop={2}>
+              <Grid
+                item
+                xl={6}
+                md={12}
+              >
                 <Card variant='outlined' sx={{ borderRadius: 0, border: 0 }}>
                   <CardHeader
                     title="The Final Hard Copy Appraisal Report :"
                   />
                   <CardContent>
+
                     <Typography>
-                    All the quality analysis information gathered from the physical inspection is documented in a methodical and clear easy to understand format in the final written appraisal report. Digital Photographs of all jewelry items are taken and included in the appraisal document. Your appraisal report will include the specific intended use and appropriate market level researched in the report, as well as explain the grading systems used. An explanation and the definition of market level used will be included, along with the instructions for the appropriate use of the appraisal report.
+                      All the quality analysis information gathered from the physical inspection is documented in a methodical and clear easy to understand format in the final written appraisal report. Digital Photographs of all jewelry items are taken and included in the appraisal document. Your appraisal report will include the specific intended use and appropriate market level researched in the report, as well as explain the grading systems used. An explanation and the definition of market level used will be included, along with the instructions for the appropriate use of the appraisal report.
                     </Typography>
+
                   </CardContent>
                 </Card>
                 <Card variant='outlined' sx={{ borderRadius: 0, border: 0 }}>
@@ -364,13 +491,28 @@ const DiamondAppraisal = () => {
                     title="Partial List of Gemological Instruments Available at DiAsecur :"
                   />
                   <CardContent>
+
                     <Typography>
-                    Diamond Proportion analyzer, Electronic Carat Weight Gem Scale, Electronic Metal Scale, Dichroscope, Polaroid Filter Lenses for Microscope, Refractometer, Day Light Diamond Grading Light, 10X Hand Loupe, Master Diamond Comparison Set / GIA, Polariscope, GIA Proportion Scope, Ideal Scope, Hearts & Arrow Scope, Heavy Testing Liquids, Fiber Optic Light Source, Chelsea Filter, Millimeter Micrometer, Presidium Electronic Gemstone Gauge Presidium Electronic Gemstone Tester, Presidium Gem Weight Scale, GIA GEM Spectroscope, G-1 Electronic Gold Tester, Ultra-Violet Light (long and short wave), Leverage Gauge and Tables, Electronic Moissanite Tester, Electronic Diamond Tester.
+                      Diamond Proportion analyzer, Electronic Carat Weight Gem Scale, Electronic Metal Scale, Dichroscope, Polaroid Filter Lenses for Microscope, Refractometer, Day Light Diamond Grading Light, 10X Hand Loupe, Master Diamond Comparison Set / GIA, Polariscope, GIA Proportion Scope, Ideal Scope, Hearts & Arrow Scope, Heavy Testing Liquids, Fiber Optic Light Source, Chelsea Filter, Millimeter Micrometer, Presidium Electronic Gemstone Gauge Presidium Electronic Gemstone Tester, Presidium Gem Weight Scale, GIA GEM Spectroscope, G-1 Electronic Gold Tester, Ultra-Violet Light (long and short wave), Leverage Gauge and Tables, Electronic Moissanite Tester, Electronic Diamond Tester.
                     </Typography>
+
                   </CardContent>
                 </Card>
-              </CardContent>
-            </Card>
+              </Grid>
+              <Grid
+                item
+                xl={6}
+                md={12}
+                container
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+              >
+                <img src={exampleReport} alt="" style={{ width: '550px', height: '750px' }} />
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
       </Box>
       <Box >
         <Footer></Footer>
