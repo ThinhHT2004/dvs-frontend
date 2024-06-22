d72dee0656dd7d0d2e7806eeeb1897bff1007203
Truong Thinh <thinhndtse182967@fpt.edu.vn>
2024-06-22 16:55:59 +0700

    tst

--- src/Components/homepage/BannerComponent.jsx
+++ src/Components/homepage/BannerComponent.jsx
@@ -58,7 +58,15 @@ const Banner = () => {
                     <Typography variant="h4" fontSize="40px">
                         Jewelers & Save "Time & Money"
                     </Typography>
-                    
+                    <Typography variant="h5" gutterBottom color="#828282">
+                        Get the best price for your diamond.
+                    </Typography>
+                    <Typography variant="h5" gutterBottom color="#828282">
+                        Get the best price for your diamond.
+                    </Typography>
+                    <Typography variant="h5" gutterBottom color="#828282">
+                        Get the best price for your diamond.
+                    </Typography>
                     <Box sx={{ display: 'flex', gap: 2, marginTop: 2 }}>
                         <Button sx={Style.Button} variant="contained" onClick={() => navigator('/calculate')}>Calculate price</Button>
                         <Button sx={Style.Button} variant="contained" onClick={() => navigator('/diamond-list')}>List of diamonds</Button>
