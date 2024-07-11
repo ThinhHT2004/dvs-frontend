2c63831efe8722ae6f34798273b9e1f44225b5a8
Nguyen Tran Tuan Khang <khangnttse183039@fpt.edu.vn>
2024-07-12 00:02:00 +0700

    Update Reset Password #2

--- src/Components/login/ResetPassword.jsx
+++ src/Components/login/ResetPassword.jsx
@@ -9,11 +9,11 @@ import {
   Typography,
   CardHeader,
   Card,
-  Link,
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { Toaster, toast } from "sonner";
 import Footer from "../footer/Footer";
+import DiasecurWhiteLogo from "../../assets/DiasecurWhiteLogo.png";
 import publicApi from "../../APIs/PublicApi";
 
 const ResetPassword = () => {
@@ -21,8 +21,8 @@ const ResetPassword = () => {
   const [submitted, setSubmitted] = useState(false);
   const navigator = useNavigate();
 
-  function handleForgetPassword() {
-    setSubmitted(true);
+  function handleResetPassword() {
+    setSubmitted(true); // Set submitted to true immediately
 
     publicApi
       .post("/auth/resetpassword", { email })
@@ -51,136 +51,62 @@ const ResetPassword = () => {
       >
         <Grid container display={'flex'} justifyContent={'center'} alignItems={'center'}>
 
-          <Card component={Paper} sx={{ width: 400, padding: 5, margin: 5 }} elevation={5}>
-            <CardHeader
-              title="Reset Password"
-              titleTypographyProps={{ variant: 'h4', align: 'center' }}
-            />
-            <Box padding={1}>
-              <TextField
-                error={email !== "" && !isValidEmail()}
-                helperText={
-                  email !== "" && !isValidEmail() ? "Email is invalid" : ""
-                }
-                label="Email"
-                placeholder="Email"
-                fullWidth
-                required
-                variant="standard"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              ></TextField>
-            </Box>
-            <Box padding={3} textAlign={'center'}>
-              <Button variant="contained" sx={{ background: '#69CEE2', borderRadius: '8px' }} onClick={() => handleLogin()} >Sign In</Button>
-            </Box>
-            <Typography padding={3} textAlign={'center'}>
-              Remembered your password? <Link href="/accounts/signin" underline='none'>Sign in</Link>
-            </Typography>
-          </Card>
 
+
+          {submitted ? (
+            <Box padding={5}>
+              <Typography variant="h4" align="center">
+                A reset password link has been sent to your email address
+              </Typography>
+              <Typography variant="h5" align="center">
+                Please check your email to reset your password
+              </Typography>
+            </Box>
+          ) : (
+            <Card component={Paper} sx={{ width: 400, padding: 5, margin: 5 }} elevation={5}>
+              <CardHeader
+                title="Reset Password"
+                titleTypographyProps={{ variant: 'h4', align: 'center' }}
+              />
+              <Box padding={1}>
+                <TextField
+                  error={email !== "" && !isValidEmail()}
+                  helperText={
+                    email !== "" && !isValidEmail() ? "Email is invalid" : ""
+                  }
+                  label="Email"
+                  placeholder="Email"
+                  fullWidth
+                  required
+                  variant="standard"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                ></TextField>
+              </Box>
+              <Box padding={3} textAlign={'center'}>
+                <Button
+                  variant="contained"
+                  sx={{ background: '#69CEE2', borderRadius: '8px', marginRight: 1 }}
+                  onClick={handleResetPassword}
+                >
+                  Reset
+                </Button>
+                <Button
+                  variant="outlined"
+                  sx={{ borderRadius: '8px' }}
+                  onClick={() => navigator("/accounts/signin")}
+                >
+                  Cancel
+                </Button>
+              </Box>  
+            </Card>
+          )}
         </Grid>
       </Box>
-      <Box>
+      <Box sx={{ mt: "auto" }}>
         <Footer></Footer>
       </Box>
-
-    </Box >
-    //       {submitted ? (
-    //         <Box padding={5}>
-    //           <Typography variant="h4" align="center">
-    //             A reset password link has been sent to your email address
-    //           </Typography>
-    //           <Typography variant="h5" align="center">
-    //             Please check your email to reset your password
-    //           </Typography>
-    //         </Box>
-    //       ) : (
-    //         <Card
-    //           component={Paper}
-    //           sx={{ width: 600, padding: 0, margin: 5, borderRadius: 2 }}
-    //           elevation={5}
-    //         >
-    //           <Grid container padding={0}>
-    //             <Grid item xs={12} padding={5}>
-    //               <CardHeader
-    //                 title="Reset Password"
-    //                 titleTypographyProps={{ variant: "h4", align: "center" }}
-    //               />
-    //               <Box padding={1}>
-    //                 <TextField
-    //                   error={email !== "" && !isValidEmail()}
-    //                   helperText={
-    //                     email !== "" && !isValidEmail() ? "Email is invalid" : ""
-    //                   }
-    //                   label="Email"
-    //                   placeholder="Email"
-    //                   fullWidth
-    //                   required
-    //                   variant="standard"
-    //                   value={email}
-    //                   onChange={(e) => setEmail(e.target.value)}
-    //                 ></TextField>
-    //               </Box>
-    //               <Box padding={3} textAlign={"center"} sx={{gap: '10px'}}>
-    //                 <Button
-    //                   variant="contained"
-    //                   sx={{ background: "#69CEE2", borderRadius: "8px" }}
-    //                   onClick={handleForgetPassword}
-    //                   disabled={!isValidEmail()}
-    //                 >
-    //                   Submit
-    //                 </Button>
-    //                 <Button
-    //                   variant="contained"
-    //                   sx={{ background: "#CECECE", borderRadius: "8px" }}
-    //                   onClick={navigator("/accounts/signin")}
-    //                 >
-    //                   Cancle
-    //                 </Button>
-    //               </Box>
-    //             </Grid>
-    //             <Grid
-    //               item
-    //               xs={12}
-    //               container
-    //               direction="column"
-    //               justifyContent="center"
-    //               alignItems="center"
-    //               sx={{ backgroundColor: "#69CEE2", padding: 0 }}
-    //             >
-    //               <Typography padding={2} textAlign={"center"} color={"white"}>
-    //                 Remember your password?
-    //               </Typography>
-    //               <Box
-    //                 sx={{
-    //                   display: "flex",
-    //                   justifyContent: "center",
-    //                   alignItems: "center",
-    //                 }}
-    //               >
-    //                 <Button
-    //                   variant="outlined"
-    //                   sx={{
-    //                     color: "white",
-    //                     borderColor: "white",
-    //                     borderRadius: 10,
-    //                   }}
-    //                   onClick={() => navigator("/accounts/signin")}
-    //                 >
-    //                   Sign In
-    //                 </Button>
-    //               </Box>
-    //             </Grid>
-    //           </Grid>
-    //         </Card>
-    //       )}
-    //     </Grid>
-    //   </Box>
-    //   <Box sx={{ mt: "auto" }}>
-    //     <Footer></Footer>
-    //   </Box>
-    // </Box>
+    </Box>
   );
 };
 
