766cab871c7dbf9d5bb970053419c3e569784f64
bmdjtt <thinhndtse182967@fpt.edu.vn>
2024-07-19 21:07:19 +0700

    change baseURL

--- src/APIs/PublicApi.js
+++ src/APIs/PublicApi.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const publicApi = axios.create(
     {
-        // baseURL: 'https://dvs-backend-production.up.railway.app/api'
-        baseURL: 'http://localhost:8080/api'///
+        baseURL: 'https://dvs-backend-production.up.railway.app/api'
+        // baseURL: 'http://localhost:8080/api'///
     }
 );
 
